84b2085eed668ee910e002ec258f72124675cdbe
Roberto M. Assandri <roberto.assandri@gmail.com>
2014-07-23 16:36:15 -0300

    Initial commit
    Stock Control app inital commit

---
+++ Entities/Model.Designer.cs
@@ -0,0 +1,2 @@
+﻿// Seeing this comment implies that the code generation for file 'd:\Robert\Documents\Visual Studio 2010\Projects\StockControl\Entities\Model.edmx'
+// failed. See the ErrorList for details.
